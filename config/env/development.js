bd385cf363de7cd2f2479a7f4f2ddb6ea7d638d3
pepatz001 <30611543+pepatz001@users.noreply.github.com>
2017-12-12 19:00:52 +0700

    Update development.js

--- config/env/development.js
+++ config/env/development.js
@@ -1,6 +1,6 @@
 module.exports = {
 
-   mongoUri: 'mongodb://localhost:27017/project',
+   mongoUri: 'mongodb://admin:password@ds129936.mlab.com:29936/e-learning',
     // or use 
      //mongoUri: 'mongodb://admin:password@localhost:27017/project',
     debug: true,
