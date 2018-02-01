e3bc5d6e924bcb62cc4626ec9422793d1cc4f31b
Sira Lownoppakul <sira.pepat@g.swu.ac.th>
2018-02-01 17:58:06 +0700

    edit model department

--- app/models/department.model.js
+++ app/models/department.model.js
@@ -3,11 +3,11 @@ var Schema = mongoose.Schema;
 
 var DepartmentSchema = new Schema({
     name: String,
-    content: [{
+    content: {
         topic: String,
         name: String,
         code: String
-    }],
+    },
     created: {
         type: Date,
         default: Date.now
